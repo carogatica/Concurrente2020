824ff929a95b0978cdf1b7c5d258775c8b408088
leluza <adelina.mayol@gmail.com>
2020-04-12 18:54:07 -0300

    clase Politica

---
+++ src/Politicas.java
@@ -0,0 +1,12 @@
+
+
+// 1) seteo de prioridades
+//que prioridad tiene cada transicion?? ---> iguales prioridad --> random
+
+//eleccion de T
+
+//eleccion de buffer
+// elegit buffer para mantener cargas equitativas  --> elige el buffer qye tenga
+//menos token, y si tienen igual cantidad, eleccion random
+
+
