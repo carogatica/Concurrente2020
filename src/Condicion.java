11042dcd6aac3504a84ec4db664696c4497ceca2
leluza <adelina.mayol@gmail.com>
2020-04-12 17:08:04 -0300

    clase condicion actualizacada

---
+++ src/Condicion.java
@@ -0,0 +1,25 @@
+public class Condicion {
+    private boolean condicion;
+
+    public Condicion(boolean condicion) {
+        this.condicion = condicion;
+    }
+
+    public void setCondicion(boolean condicion) {
+        this.condicion = condicion;
+        if(condicion){
+           h1.finalizar();
+           h2.finalizar();
+           h3.finalizar();
+           h4.finalizar();
+           h5.finalizar();
+           h6.finalizar();
+           h7.finalizar();
+           h8.finalizar();
+        }
+    }
+    public boolean getCondicion(){
+        return condicion;
+    }
+
+}
