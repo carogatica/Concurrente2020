49769b3408f95bd0c56ff4604c8d9c27e89fc506
leluza <adelina.mayol@gmail.com>
2020-06-06 15:28:53 -0300

    Funciona lA concurrencia. Resta testear invariantes y el momento de
    interrupcion de los hilos

--- src/Monitor.java
+++ src/Monitor.java
@@ -17,10 +17,15 @@ public class Monitor {
     // private ArrayList<Integer> vSensibilizadas;
     private ArrayList<Integer> vSensibilizadas;
 
+    private int[] vvSensibilizadas;
+
     private int[] vColas;
     private int[] m;
     private boolean k;
+    private boolean test_on = true;
 
+    
+    
     public Monitor(RedDePetri red, Politicas politicas, Condicion condicion) {
         this.RdP = red;
         this.VariablesDeCondicion = new Cola[RdP.getCantTransiciones()]; // se generan tantas variables de condicion
@@ -46,43 +51,150 @@ public class Monitor {
         //k = RdP.esSensibilizada(transicion) && RdP.disparar(transicion);;
         while (k) {
             //k = RdP.disparar(transicion);
-            k = RdP.esSensibilizada(transicion) && RdP.disparar(transicion);;
-            System.out.println("Transicion: " + transicion);
-            if (k) {
+            k = RdP.esSensibilizada(transicion) && RdP.disparar(transicion);
+            if(test_on) System.out.println("Intento T: " + transicion +  " " + leyendas(transicion));
+            if (k) {//disparo
+            	if(test_on)	System.out.println("Disparada: " + transicion + " " + leyendas(transicion));
+	                vSensibilizadas = RdP.getSensibilizadas();
+	                vvSensibilizadas = new int[RdP.getCantTransiciones()];
+	               // for (int i = 0; i < vvSensibilizadas.length; i++) {
+	                   for ( Integer orden :  vSensibilizadas )
+	                   {
+	                	   vvSensibilizadas[orden] = 1;
+	                   }
+	               // }
+	                vColas = new int[RdP.getCantTransiciones()];
+	                m = new int[RdP.getCantTransiciones()];
+	                //quienesEstan( vColas);
+	                
+	                for (int i = 0; i < VariablesDeCondicion.length; i++) {
+	    	            if (this.VariablesDeCondicion[i].Empty()) {
+	    	                vColas[i] = 0;
+	    	            } else {
+	    	                vColas[i] = 1;
+	    	            }
+	    	
+	    	        }
+	                
+	                for (int i = 0; i < vvSensibilizadas.length; i++) {
+	 //               for (int i = 0; i < vSensibilizadas.size(); i++) {
+	          //      	if (vSensibilizadas.get(i) == 1 && vColas[i] == 1) {
+	                  	if ((vvSensibilizadas[i] == 1) && (vColas[i] == 1)) {
+	                  		m[i] = 1;
+	                        cont++;
+	                        auxIndice = i;
+	                    } else {
+	                        m[i] = 0;
+	                    }
+	                }
+	
+	                if(test_on) imprimir_vector(vvSensibilizadas,"vvSensibilizadas ");
+	                if(test_on) imprimir_vector(vColas,          "vColas           ");
+	                if(test_on) imprimir_vector( m,              " m               ");
+	                if(test_on) System.out.println("");
+	                if(test_on) if(cont > 0) { System.out.println("cont: " + cont);}
+	                
+	                if (cont == 0)
+	                    k = false;
+	
+	                if (cont == 1) {
+	                	if(test_on) System.out.print( "UP --> "+  auxIndice);
+	                    VariablesDeCondicion[ auxIndice].Resume();
+	                    return;}
+	                    
+	
+	                if (cont > 1) {
+	                    seleccionado = politica.cual(m);
+	                    //no concidera el caso de las T que no alimentan los buffers
+	                    //en que momento se usan las prioridadeS??
+	                    VariablesDeCondicion[seleccionado].Resume();
+	                	if(test_on) System.out.print( "UP sel --> "+ seleccionado);
+	                	return;
+	                }
+	
+	                cont = 0;
+	                    
+            } // if SI disparop
+            else
+            {// No disparo
+            	mutex.release(); // devuelve mutex
+                VariablesDeCondicion[transicion].Delay();
 
-                vSensibilizadas = RdP.getSensibilizadas();
-                vColas = new int[RdP.getCantTransiciones()];
-                m = new int[RdP.getCantTransiciones()];
-
-                vColas = quienesEstan();
-                for (int i = 0; i < vSensibilizadas.size(); i++) {
-                    if (vSensibilizadas.get(i) == 1 && vColas[i] == 1) {
-                        m[i] = 1;
-                        cont++;
-                        auxIndice = i;
-                    } else {
-                        m[i] = 0;
-                    }
-                }
-                if (cont == 0)
-                    k = false;
-
-                if (cont == 1)
-                    VariablesDeCondicion[transicion].Resume();
-
-                if (cont > 1) {
-                    seleccionado = politica.cual(m);
-                    VariablesDeCondicion[transicion].Resume();
-                }
-
-                cont = 0;
-                System.out.println("Disparada: " + transicion);
-            } 
-        }
-        actualizarCondiciones(transicion);
+            }//else
+            
+        }//while
         mutex.release(); // devuelve mutex
+        actualizarCondiciones(transicion);
 
     }
+    
+    private void imprimir_vector( int[] v, String nombre)
+    {
+    	System.out.print( " " + nombre + ": [");
+    	
+	    	int suma = 0;
+    	for( int i = 0; i< v.length; i++ )
+			{
+				 suma = suma +v[i];   
+			}
+	    	
+    	if( suma > 0) {
+    		for( int i = 0; i< v.length; i++ )
+    		{
+    			if( v[i] == 0 ) {
+	    			if( i == (v.length - 1) )		System.out.println( v[i] + "] ");
+	    			else 	System.out.print( v[i] + ", ");
+    			}else
+    			{
+    				if( i == (v.length - 1) )		System.out.println( "X"+ "] ");
+	    			else 	System.out.print("X"+ ", ");
+    			}
+    		}
+    	}else
+    	{
+    		System.out.println(" nulo");
+    	}
+    }
+    
+    private String leyendas(int transicion)
+    {
+        String detale;
+
+        switch(transicion)
+        {
+        case 0:		detale = " tarea 1/2 A"; break;
+        case 4:		detale = " tarea 2/2 A"; break; 
+        
+        case 3: 	detale = " Nucleo 1/2 A"; break;
+        case 5: 	detale = " Nucleo 2/2 A"; break;
+        
+        case 7: 	detale = " CPU 1/3 A"; break;
+        case 2: 	detale = " CPU 2/3 A"; break;
+        case 1: 	detale = " CPU 3/3 A"; break;
+        
+        case 6: 	detale = " KeepLive 1 A"; break;
+        
+        case 14:	detale = " tarea 2/2 B"; break; 
+        
+        case 13: 	detale = " Nucleo 1/2 B"; break;
+        case 12: 	detale = " Nucleo 2/2 B"; break;
+        
+        case 11: 	detale = " CPU 1/3 B"; break;
+        case 9: 	detale = " CPU 2/3 B"; break;
+        case 8: 	detale = " CPU 3/3 B"; break;
+        
+        case 10: 	detale = " KeepLive B"; break;
+        
+        
+        default:    detale = "nose";break;
+        
+        }//case
+        
+        return detale;
+
+    	
+    }
+    
 
     private void GenerarVarCond() { // crea tantas variables de condicion como cantidad de transiciones tiene la red
                                     // de petri
@@ -91,16 +203,18 @@ public class Monitor {
         }
     }
 
-    private int[] quienesEstan() {
-        for (int i = 0; i < VariablesDeCondicion.length; i++) {
-            if (this.VariablesDeCondicion[i].Empty()) {
-                vColas[i] = 0;
-            } else {
-                vColas[i] = 1;
-            }
-
-        }
-        return vColas;
+   // private int[] quienesEstan() {
+     private void quienesEstan( int[] Colas) {
+	    
+	    for (int i = 0; i < VariablesDeCondicion.length; i++) {
+	            if (this.VariablesDeCondicion[i].Empty()) {
+	                Colas[i] = 0;
+	            } else {
+	                Colas[i] = 1;
+	            }
+	
+	        }
+      //  return vColas;
     }
 
     private void actualizarCondiciones(int transicion) {// lleva la cuenta de las tareas que se hacen en cada
@@ -114,8 +228,8 @@ public class Monitor {
             System.out.println("Llega");
             // desbloquearTodos();
             condicionDeFinalizacion.setCondicion(true);
-            System.out.println("Tareas completadas en N1: " + finalN1);
-            System.out.println("Tareas completadas en N2: " + finalN2);
+            System.out.println("*Tareas completadas en N1: " + finalN1);
+            System.out.println("*Tareas completadas en N2: " + finalN2);
             RdP.printArchivo(finalN1, "Tareas completadas en N1");
             RdP.printArchivo(finalN2, "Tareas completadas en N2");
         }
